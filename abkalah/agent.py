852eeed76a6b38665ed0f2579a2d307d239ce8c5
LiNoimi Semain <iplay.addicted@gmail.com>
2018-12-09 19:03:47 +0000

    Create while loop for playing

--- abkalah/agent.py
+++ abkalah/agent.py
@@ -5,6 +5,7 @@ from threading import Thread
 
 from abkalah import ab_break, ab_lock, best_move, NORTH, SOUTH
 from abkalah.game.board import Board
+from abkalah.game.alphabeta import AlphaBeta
 
 class Agent:
   def __init__(self):
@@ -20,24 +21,9 @@ class Agent:
       self.side = NORTH if message[6] == 'N' else SOUTH
       self.playing = self.side == SOUTH
 
-      # start search
+      while self.playing: self._play()
+
       self._start_ab()
-
-      if self.playing:
-        # break after 1 second
-        self._wait_for_ab(1)
-        
-        next_move = best_move if self.side == NORTH else best_move - 8
-
-        # send message to stdout
-        sys.stdout.write('MOVE;' + str(next_move) + '\n')
-        sys.stdout.flush()
-
-        # update board with our move
-        self.board = self.board.move(best_move)
-
-        # start search
-        self._start_ab()
 
     elif message[:6] == 'CHANGE':
       # break search and wait for result
@@ -48,30 +34,18 @@ class Agent:
         self.playing = not self.playing
 
         self._start_ab()
-        return
+      else:
+        # update board with opponent move
+        op_side = NORTH if self.side == SOUTH else SOUTH
+        move = int(message[7])
+        move = move if op_side == NORTH else 14 - move
 
-      # update board with opponent move
-      op_side = NORTH if self.side == SOUTH else SOUTH
-      move = int(message[7])
-      move = move if op_side == NORTH else move + 8
+        self.board = self.board.move(move)
 
-      self.board = self.board.move(move)
+        while self.playing: self._play()
 
-      # interative depth search
-      self._start_ab()
-      self._wait_for_ab(1)
-      
-      next_move = best_move if self.side == NORTH else best_move - 8
-
-      # send message to stdout
-      sys.stdout.write('MOVE;' + str(next_move) + '\n')
-      sys.stdout.flush()
-
-      # update board with our move
-      self.board = self.board.move(best_move)
-
-      # start search
-      self._start_ab()
+        # start search
+        self._start_ab()
 
     else: # END
       self._wait_for_ab(0)
@@ -90,6 +64,21 @@ class Agent:
     ab_break = True
     ab_lock.acquire()
     ab_lock.release()
+  
+  def _play(self):
+    # interative depth search
+    self._start_ab()
+    self._wait_for_ab(1)
+    
+    next_move = best_move if self.side == NORTH else 14 - best_move
+
+    # send message to stdout
+    sys.stdout.write('MOVE;' + str(next_move) + '\n')
+    sys.stdout.flush()
+
+    # update board with our move
+    self.board, next_player = self.board.move(best_move)
+    self.playing = next_player == self.side
 
   def calculate(self):
     global best_move
