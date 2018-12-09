e25d8393b8ec0acfa571790df158edfbfa81d8ca
razvan.lacatusu <razvan.lacatusu@thehutgroup.com>
2018-12-09 17:58:39 +0000

    Game_Logic move array created

--- abkalah/game/board.py
+++ abkalah/game/board.py
@@ -13,16 +13,20 @@ class Board:
     # check where the last one is
     # if it is on our side, keep the same player
     # if not change player
+
+    #inherit values from previous board
+    moved_board = [ i for i in self.board ]
+
     opponent = NORTH if move < 8 else SOUTH
     player = NORTH if opponent == SOUTH else SOUTH
 
-    beans = self.board[move - 1]
+    beans = moved_board[move - 1]
     current_well = move - 1
 
     if beans == 0:
       raise ValueError("Invalid move")
 
-    self.board[move - 1] = 0
+    moved_board[move - 1] = 0
     current_well += 1
 
 
@@ -31,48 +35,51 @@ class Board:
       # Inside own kalaha
         if player_side == player:
         # Own kalaha - sow
-          self.board[current_well] += 1
+          moved_board[current_well] += 1
           beans -= 1
           if beans == 0:
           # Turn ended inside own kalaha => new turn for player
           # or victory if no more moves left
             ending = 1
             for position in range(1, WELLS):
-              if self.board[current_well - position] > 0:
+              if moved_board[current_well - position] > 0:
                 ending = 0
 
             if ending == 1:
-              for opp in range(1, (WELLS - 1)):
-                if self.board[current_well + opp] > 0:
-                  self.board[current_well] += self.board[current_well + opp]
-                  self.board[current_well + opp] = 0
-            return Board(), player
+              for opp in range(1, WELLS):
+                if moved_board[current_well + opp] > 0:
+                  moved_board[current_well] += moved_board[current_well + opp]
+                  moved_board[current_well + opp] = 0
+            return Board(moved_board), player
 
         current_well += 1
         player_side = NORTH if player_side == SOUTH else SOUTH
       else:
       # Inside wells
         beans -= 1
-        self.board[current_well] += 1
+        moved_board[current_well] += 1
         if beans == 0:
         # Turn ended
-          if player_side == player and self.board[current_well] == 1:
+          if player_side == player and moved_board[current_well] == 1:
             # Ended in own, empty current well
             opposite_well = 16 - current_well
-            self.board[(8 * player) - 1] += (self.board[current_well] + self.board[opposite_well])
-            self.board[current_well] = self.board[opposite_well] = 0
+            moved_board[(8 * player) - 1] += (moved_board[current_well] + moved_board[opposite_well])
+            moved_board[current_well] = moved_board[opposite_well] = 0
           # Game ended - opponent wins?
             ending = 1
             for position in range(1, WELLS):
-              if self.board[current_well - position] > 0:
+              if moved_board[((8 * player) - 1) - position] > 0:
                 ending = 0
 
             if ending == 1:
               for opp in range(0, (WELLS - 1)):
-                if self.board[opp] > 0:
-                  self.board[(8 * opponent) - 1] += self.board[opp]
-                  self.board[opp] = 0
-            return Board(), opponent
+                if moved_board[opp] > 0:
+                  moved_board[(8 * opponent) - 1] += moved_board[opp]
+                  moved_board[opp] = 0
+            return Board(moved_board), opponent
         current_well += 1
 
-  def available_moves():
+  def available_moves(self, player):
+    pass
+
+
