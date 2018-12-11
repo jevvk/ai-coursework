cdd8ed1461765851987e6a2ddc23762f4b42bd28
LiNoimi Semain <iplay.addicted@gmail.com>
2018-12-11 20:37:46 +0000

    Reformat and fix evaluation

--- abkalah/agent/evaluate.py
+++ abkalah/agent/evaluate.py
@@ -4,65 +4,73 @@ from abkalah import NORTH, SOUTH
 from abkalah.game.board import Board
 
 def evaluate(board, player):
-    score = 0
-    p1_stones = board.count_stones(NORTH)
-    p2_stones = board.count_stones(SOUTH)
+  score = 0
 
-    score = (board.state[7] - board.state[15]) + (p1_stones - p2_stones)
+  p1_stones = board.count_stones(NORTH)
+  p2_stones = board.count_stones(SOUTH)
 
-    if board.state[7] > 49:
-        score += 10000
-    elif board.state[15] > 49:
-        score -= 10000
+  score = (board.state[7] - board.state[15]) * 5 + (p1_stones - p2_stones) * 1.5
 
-    mostOpponent = 0
-    ai_min = 9999999 # TODO
-    ai_most = -9999999 # TODO
-    for i in range(0,7):
-        oppositeIndex = 14 - i
-        if board.state[i] < ai_min:
-            ai_min = board.state[i]
+  if board.state[7] > 49:
+    score += 10000
+  elif board.state[15] > 49:
+    score -= 10000
 
-        if board.state[i] > ai_most:
-            ai_most = board.state[i]
+  mostOpponent = 0
+  ai_min = 98
+  ai_most = 0
 
-        if board.state[i] == 0:
-            if board.state[oppositeIndex] > 0:
-                canSteal = 0
-                for j in range(0, 7):
-                    if ((board.state[j] - (i-j)) % 15) == 0:
-                        canSteal = 1
-                if canSteal:
-                    score += board.state[oppositeIndex]
+  for i in range(0,7):
+    oppositeIndex = 14 - i
 
-        if board.state[oppositeIndex] > mostOpponent:
-            mostOpponent = board.state[oppositeIndex]
+    if board.state[i] < ai_min:
+      ai_min = board.state[i]
 
-        if board.state[oppositeIndex] == 0:
-            canOpponentSteal = 0
-            if board.state[i] > 0:
-                for j in range (8, 15):
-                    if (board.state[j] - (i-j)) % 15 == 0:
-                        canOpponentSteal = 1
-                if canOpponentSteal:
-                    score -= board.state[i]
+    if board.state[i] > ai_most:
+      ai_most = board.state[i]
 
-    if (ai_most - ai_min) > 10:
-        score -= 1
+    if board.state[oppositeIndex] > mostOpponent:
+      mostOpponent = board.state[oppositeIndex]
 
-    ratio = float(p1_stones) / float(p2_stones)
+    if board.state[i] == 0:
+      canSteal = 0
 
-    if ratio < .72:
-        score -= mostOpponent * 2
+      if board.state[oppositeIndex] > 0:
+        for j in range(0, 7):
+          if ((board.state[j] - (i-j)) % 15) == 0 and i != j:
+            canSteal = 1
 
-    inverseRatio = float(p2_stones) / float(p1_stones)
-    if inverseRatio > .85:
-        score -= mostOpponent / 2
+        if canSteal:
+            score += board.state[oppositeIndex] * 1.75
 
-    score += board.get_free_turns_for_player(NORTH)
-    score -= board.get_free_turns_for_player(SOUTH)
+    if board.state[oppositeIndex] == 0:
+      canOpponentSteal = 0
 
-    if player == SOUTH:
-        score *= -1
+      if board.state[i] > 0:
+          for j in range (8, 15):
+            if (board.state[j] - (i-j)) % 15 == 0 and oppositeIndex != j:
+              canOpponentSteal = 1
 
-    return score
+          if canOpponentSteal:
+              score -= board.state[i] * 1.75
+
+  if (ai_most - ai_min) > 7:
+    score -= ai_most
+
+  ratio = float(p1_stones) / float(p2_stones)
+
+  if ratio < .72:
+    score -= mostOpponent * 2
+
+  inverseRatio = float(p2_stones) / float(p1_stones)
+
+  if inverseRatio > .85:
+    score -= mostOpponent / 2
+
+  score += board.get_free_turns_for_player(NORTH) * 3
+  score -= board.get_free_turns_for_player(SOUTH) * 3
+
+  if player == SOUTH:
+    score *= -1
+
+  return score
