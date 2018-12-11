0be856efe87f92cbb9ca05316d4b92b6d5e35130
LiNoimi Semain <iplay.addicted@gmail.com>
2018-12-11 13:29:14 +0000

    Add 2 more methods to table

--- abkalah/game/table.py
+++ abkalah/game/table.py
@@ -15,5 +15,15 @@ class TTable:
   def put(self, player, board, value):
     self.table[self._hash(board, player)] = value
 
+  # TODO: given p1 and p2 stones, remove all evaluations which
+  # have less stones since they can't be reached anymore
+  def clean(self, p1_stones, p2_stones):
+    pass
+
+  # TODO: remove all evaluations
+  def reset(self):
+    pass
+
+  # TODO
   def _hash(self, player, board):
-    return 0 # TODO
+    return 0
